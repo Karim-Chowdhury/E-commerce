d0adb884ec4bbdae6f256dd34ce8425da2e3bbc9
Karim <karimchowdhury45@gmail.com>
2023-12-09 15:57:11 +0600

    cart page updated

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -26,44 +26,44 @@ const CartItemPage = () => {
     <Layout>
       <div className="p-5">
         <div className="container lg:w-2/3 xl:w-2/3 mx-auto">
-          <h1 className="text-3xl font-bold mb-6">Your Cart Items</h1>
-          <div className="bg-white p-4 rounded-lg shadow">
+          <h1 className="text-3xl font-bold mb-6 text-primary text-center pb-4">Your Cart Items</h1>
+          <div className="bg-white p-4 rounded-lg shadow dark:bg-gray-800">
             <div>
               <div>
                 <div>
                   {
                     cartList.map((item: any, index: number) => (
-                      <div key={index} className="w-full flex flex-col sm:flex-row items-center gap-4">
+                      <div key={index} className="w-full flex flex-col sm:flex-row items-center gap-4 p-4">
                         <Link
                           href={`${baseUrl}/product/${item?.product_slug}`}
-                          className="w-36 h-32 flex items-center justify-center overflow-hidden"
+                          className=" w-36 h-37 flex items-center justify-center overflow-hidden rounded-md"
                         >
                           <img
                             src={`${baseUrl}/${item?.product_images [0] ?.image}`}
                             alt=""
-                            className="object-cover"
+                            className="object-cover "
                           />
                         </Link>
                         <div className="flex-1 flex flex-col justify-between">
                           <div className="flex justify-between mb-3">
-                            <h3 className="text-lg text-black">{item?.product_title}</h3>
+                            <h3 className="text-lg text-primary  font-semibold">{item?.product_title}</h3>
                             <span className="text-lg font-semibold text-black">
                               $<span>{item?.total_price}</span>
                             </span>
                           </div>
                           <div className="flex justify-between items-center">
-                            <div className="flex items-center text-black">
+                            <div className="flex items-center text-black dark:text-white ">
                               Qty:
                               <input
                                 type="number"
                                 onChange={(e) => quantityUpdate(item?.product_id, e.target.value, item?.product_price)}
-                                className="ml-3 py-1 border-gray-200 focus:border-purple-600 focus:ring-purple-600 w-16"
+                                className="ml-3 py-1 border-gray-300 focus:border-accentOne focus:ring-accensOne w-15 rounded-md text-black"
                                 value={item?.quantity}
                               />
                             </div>
                             <button
                               onClick={() => removeCart(item?.product_id)}
-                              className="text-purple-600 hover:text-purple-500"
+                              className="text-primary border border-primary px-2 py-1 rounded hover:bg-accentTwo hover:text-white"
                             >
                               Remove
                             </button>
@@ -81,15 +81,13 @@ const CartItemPage = () => {
             {/*/ Product Items */}
             <div className="border-t border-gray-300 pt-4">
               <div className="flex justify-between">
-                <span className="font-semibold">Subtotal</span>
-                <span className="text-xl">$59</span>
+                <span className="font-bold text-primary">Subtotal</span>
+                <span className="text-xl text-primary">$59</span>
               </div>
               <p className="text-gray-500 mb-6">
                 Shipping and taxes calculated at checkout.
               </p>
-              <button type="submit" className="btn-primary w-full py-3 text-lg">
-                Proceed to Checkout
-              </button>
+             
             </div>
           </div>
         </div>
